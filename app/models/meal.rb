bc7ba4fa261e4ad22d44af10af5d204c13e7dcd7
andre ferrer <andrerferrer@hotmail.com>
2021-06-15 09:20:19 -0300

    add associations to meal model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,7 @@
 class Meal < ApplicationRecord
   belongs_to :restaurant
+  has_many :cart_items
+  has_many :shopping_carts, through: :cart_items
 
   validates :price_in_cents, presence: true
   validates :name, presence: true
