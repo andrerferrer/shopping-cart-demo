924622e4ffdd4219d539a8833bc5d88382c269c4
andre ferrer <andrerferrer@hotmail.com>
2021-06-15 10:30:59 -0300

    add route to shopping cart show

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   resources :meals, only: %i[] do
     resources :cart_items, only: %i[ create ]
   end
+
+  resources :shopping_carts, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
