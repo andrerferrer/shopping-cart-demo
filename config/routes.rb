ba31bb3f302a6af99571008f3124c20e7e8f3d03
andre ferrer <andrerferrer@hotmail.com>
2021-06-15 09:52:34 -0300

    add route to create a cart_item

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
     end
     resources :reviews, only: :create
   end
+
+  resources :meals, only: %i[] do
+    resources :cart_items, only: %i[ create ]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
